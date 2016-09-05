0b1ef519e6a2b2f7650c62bbfcf5dc16f2289794
mazdeh <vahid.mazdeh@gmail.com>
2016-09-05 13:03:33 -0600

    Promise returns daily/weekly/yearly contributions. Package.json updated. Username can be passed in, instead of read from .gitconfig.

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
 var https = require('https');
 var fs = require('fs');
 var username = require('git-user-name');
+var cheerio = require('cheerio');
 
-console.log('username: ', username());
-
+var user = username();
+var url = 'https://github.com/users/' + user + '/contributions';
 
 var body;
-https.get('https://github.com/users/mazdeh/contributions', function(res) {
-	console.log(res.statusCode);
-	res.setEncoding('utf8')
-	res.on('data', function(data) {
-		body += data;
+var $;
+var days;
+
+var getDays = function(username) {
+	return new Promise(function(resolve, reject) {
+		if (username) {
+			url = 'https://github.com/users/' + username + '/contributions';
+		}
+		https.get(url, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function(data) {
+				body += data;
+			})
+
+			if (res.statusCode !== 200) {
+				reject(res.statusCode)
+			}
+
+			res.on('end', function() {
+				$ = cheerio.load(body, { ignoreWhitespace: true, decodeEntities: true });
+				var weeks = $('g', 'g');
+				days = $('rect', weeks);
+				resolve(days);
+			})
+		}).on('error', function(err) {
+			reject(err);
+		})
 	})
-	res.on('end', function() {
-		fs.writeFile('html.txt', body);
+}
+
+exports.yearly = function(username) {
+	return new Promise(function(resolve, reject) {
+		var yearCount = 0;
+		getDays(username).then(function(days) {
+			for (var i = 0; i < days.length; i++) {
+				yearCount += parseInt($(days[i]).attr('data-count'));
+			}
+			resolve(yearCount);
+		})
+		.catch((err) => {
+			error(err);
+		})
 	})
-})
+}
+
+exports.weekly = function(username) {
+	return new Promise(function(resolve, reject) {
+		var weekCount = 0;
+		getDays(username).then((days) => {
+			for (var i = days.length -1; i > days.length - 7; i--) {
+				weekCount += parseInt($(days[i]).attr('data-count'))
+			}
+			resolve(weekCount);
+		})
+		.catch((err) => {
+			error(err);
+		})
+	})
+}
+
+exports.daily = function(username) {
+	return new Promise((resolve, reject) => {
+		var dayCount = 0;
+		getDays(username).then((days) => {
+			dayCount = parseInt($(days[days.length-1]).attr('data-count'));
+			resolve(dayCount);
+		})
+		.catch((err) => {
+			error(err);
+		})
+	})
+}
+
+var error = function(err) {
+	console.log('ERROR: ', err);
+	console.log('Error getting your github info. Please make sure you have the correct username in ~/.gitconfig.');
+}
